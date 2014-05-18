8ff9d9fc22f7e997e2a3ca0e7c40a3d3eb661490
Alin Eugen Deac <aedart@gmail.com>
2014-05-18 15:15:16 +0200

    Implemented #50: Var dump print

--- output/Output.php
+++ output/Output.php
@@ -17,18 +17,81 @@ class Output extends Task{
      * The messsage to be printed in the
      * given console window
      * 
+     * OR - can also be an object, in which
+     * case the _varDump should be set to
+     * true
+     * 
      * @var string
      */
     private $_msg = "";
     
+    /**
+     * If true, this task will print
+     * using PHP's var_export, instead
+     * of just printing the message
+     * 
+     * @var mixed
+     */
+    private $_varDump = false;
+    
+    /**
+     * Name of a property that should be
+     * var dumped
+     * 
+     * @var string
+     */
+    private $_propertyName;
+    
     public function main(){
-	print($this->_msg . PHP_EOL);
+	// Normal output
+	print((string) $this->_msg . PHP_EOL);
+	
+	// Check if a property name has been provided
+	if(!empty($this->_propertyName)){
+	    // We use the getProperties, to avoid eventual
+	    // parsing issues with the property, should it be
+	    // used in another context
+	    $prop = $this->project->getProperties()[$this->_propertyName];
+	    if(is_null($prop)){
+		throw new BuildException("Property '" . $this->_propertyName . "' could not be found. Cannot var dump given property", $this->getLocation());
+	    }
+	    
+	    // Determine if the property should be var dumped or
+	    // just printed out as it is (string conversion attempt)
+	    if($this->_varDump){
+		var_dump($prop);
+	    } else {
+		print((string) $prop . PHP_EOL);
+	    }
+	}
+    }
+    
+    /**
+     * Flag - if a given property should be var dumped
+     * 
+     * @param boolean $dump
+     */
+    public function setVarDump($dump){
+	$this->_varDump = (bool) $dump;
+    }
+    
+    /**
+     * name of a property that should be var dumped
+     * 
+     * @see setVarDump
+     * 
+     * @param string $name
+     */
+    public function setPropertyName($name){
+	$this->_propertyName = (string) $name;
     }
     
     /**
      * Setter for the attr 'message'
      * 
-     * @param string $msg
+     * NOTE: message can now actually be some other property
+     * 
+     * @param srting $msg
      */
     public function setMessage($msg) {
         $this->_msg = (string) $msg;
@@ -52,7 +115,7 @@ class Output extends Task{
      * 
      * @see EchoTask
      */
-    function addText($msg){
+    function addText($msg){	
 	// Trim message
 	$m = trim($msg);
 	
